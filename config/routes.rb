40cee84b7a27d87662f8a208c5a7d927e24df81d
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-04-29 18:54:40 +0900

    Add user controller show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   root to: 'homes#top'
-  resources :books, only: [:create, :index, :show, :destroy]
+  devise_for :users
+  resources :books, only: [:show, :index, :edit, :destroy]
+  resources :users, only: [:show]
 end
