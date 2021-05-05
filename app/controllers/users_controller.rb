432b07943e0bc25494c9e52776bc23b05f8569e6
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-05 18:35:46 +0900

    Update userRoute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @books = Book.where(user_id: current_user.id)
     @book = Book.new
   end
