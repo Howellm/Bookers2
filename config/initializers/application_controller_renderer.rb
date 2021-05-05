df2b4a9b730ae4cc0b344fd9ead84e5dde1c6201
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-05 17:27:25 +0900

    Update refilekey

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f484f4a4edf62c835fdeac594e790ea797f1907b98fcf5c37ffe354e9a346ff50cbf82dc2fae68d096148ab5b230713a0d770bda5d2d80b67406e382ea5707d4'
