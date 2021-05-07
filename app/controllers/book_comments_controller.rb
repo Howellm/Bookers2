4b63f75df2bed20c790fd43b8d8f8836026ac634
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-07 21:43:59 +0900

    Add BookComment

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,28 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(@book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_back(fallback_location: book_path(@book))
+    else
+      @book = Book.new
+      @book_detail = Book.find(params[:book_id])
+      @user = User.find(@book_detail.user_id)
+      render 'books/show'
+    end
+  end
+
+  def destroy
+    BookComment.find_by(book_id: params[:book_id]).destroy
+    redirect_back(fallback_location: book_path(params[:book_id]))
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
