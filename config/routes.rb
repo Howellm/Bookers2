58f1137e848962b350dad99190dbe8a1bb939789
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-07 18:35:21 +0900

    Update spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   devise_for :users
-  get '/about', to: 'homes#about'
+  get 'home/about', to: 'homes#about'
   resources :books, only: [:create, :show, :index, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit,:update]
 end
