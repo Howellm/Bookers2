acd8a8b4097d8390b5c0fe6df5deb4c4226fec26
Uzumal <kazuma.takeuchi.1027@gmail.com>
2021-05-07 21:43:00 +0900

    Update BookComment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.new
+    @book_comment = BookComment.new
     @book_detail = Book.find(params[:id])
     @user = User.find(@book_detail.user_id)
   end
